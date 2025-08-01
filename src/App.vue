8276bd911a517f39fc4cf5941e71ae4634a8fb1b
kebab-casee <bombelatz@gmail.com>
2025-08-01 23:06:29 +0800

    ugma nani oi yawa kakapoy

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
+<template>
+  <Navbar />
+</template>
+
 <script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-
-const users = ref([])
-const loading = ref(false)
-
-async function fetchUsers() {
-  loading.value = true
-  try {
-    const res = await axios.get('https://api.com/users')
-    users.value = res.data
-  } catch (e) {
-    console.error('Error loading users:', e)
-  } finally {
-    loading.value = false
-  }
-}
+import Navbar from '../src/components/Navbar.vue';
 </script>
 
-<template>
-  <button
-  class="bg-red-300 m-100 p-2 text-white rounded-2xl cursor-pointer hover:bg-red-100 transition-all active:bg-amber-200"
-  @click="fetchUsers">Load Users</button>
-  <div
-  class="-m-120 bg-amber-400"
-  v-if="loading">Loading...</div>
-  <ul>
-    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
-  </ul>
-</template>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import "./style.css";
-import ContactUs from "./components/contact-us.vue";
-import ButtonCounter from "./components/button-counter.vue";
 
 const app = createApp(App);
-
-app.component("contact-us", ContactUs);
-app.component("button-counter", ButtonCounter);
 
 app.mount("#app");
